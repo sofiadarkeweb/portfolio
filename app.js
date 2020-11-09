4107ae3bf76f91dd811d4233ee2dc80d62a7c4c1
Sofia Darke <sofia.darke@hyperisland.se>
2020-11-09 21:38:51 +0100

    pop up

--- app.js
+++ app.js
@@ -22,3 +22,49 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+const adjectives = ["Moody", "Delerious", "Crazy"];
+const verbs = ["Ninja", "Doll", "Apple"];
+const generatorBtn = document.querySelector(".generator-btn");
+const nameOneText = document.querySelector(".name1");
+const nameTwoText = document.querySelector(".name2");
+const answerText = document.querySelector(".answer");
+
+// eventlisterners
+generatorBtn.addEventListener("click", generateName);
+
+// generatorBtn.addEventListener("click", addAnswerText);
+
+//functions
+function generateName(){
+    generatorBtn.style.color = "transparent";
+    generatorBtn.style.fontSize = "50%";
+    const nameOne = adjectives[Math.floor(Math.random()*adjectives.length)];
+    nameOneText.innerText = nameOne;
+    const nameTwo = verbs[Math.floor(Math.random()*verbs.length)];
+    nameTwoText.innerText = nameTwo;
+    const answer = ["Not you? Try again", "Fits perfect, no?"]
+    answerText.innerText = answer[Math.floor(Math.random()*answer.length)];
+    
+    
+    if (nameOne == nameTwo) {
+        console.log('samma namn');
+        generateName();
+    }     
+}
+
+// function addAnswerText(){
+//     const answer = "Not you?"
+//     answerText.innerText = answer;
+// }
+
+// function changeButtonText(){
+//     const generatorBtn = "Not you?"
+//     generatorBtn.innerText.value = newButtonText;
+// }
+
+// changeButtonText();
+
+
+
+// addAnswerText();
